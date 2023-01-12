947adbddac2f52757bc5689a2a55573b4abdce6a
Fulya <fulya.ertay@gmail.com>
2023-01-12 22:50:10 +0300

    routing if 404

--- server.js
+++ server.js
@@ -7,8 +7,15 @@ const server=http.createServer((req,res)=>{
     // res.setHeader("Content-Type","text/html")
     // res.write("<h1>Hello NodeJS</h1>")
     // res.end();
-    res.writeHead(200,{"Content-Type":"application/json"});
-    res.end(JSON.stringify(datas))
+    if(req.url==='/api/users' && req.method==='GET'){
+        res.writeHead(200,{"Content-Type":"application/json"});
+        res.end(JSON.stringify(datas))
+
+    }else{
+        res.writeHead(404,{"Content-Type":"application/json"});
+        res.end(JSON.stringify({"message":"Not Found!"}))
+    }
+
 })
 const PORT=process.env.PORT || 5000;
 server.listen(PORT,()=>{
