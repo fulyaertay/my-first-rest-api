c050b22c791c81a32f5df88bdc951992b2cbbbde
Fulya <fulya.ertay@gmail.com>
2023-01-13 00:41:41 +0300

    added methods

--- server.js
+++ server.js
@@ -7,15 +7,17 @@ const server=http.createServer((req,res)=>{
     // res.setHeader("Content-Type","text/html")
     // res.write("<h1>Hello NodeJS</h1>")
     // res.end();
+    //GET METHOD
     if(req.url==='/api/users' && req.method==='GET'){
         getUsers(req,res)
 
+    //GET by User ID
     }else if(req.url.match(/\api\/user\/([0-9]+)/)&& req.method==='GET'){
         const id=req.url.split('/')[3];
         getUser(req,res,id)
 
     }
-    
+    //If 404, return error message
     else{
         res.writeHead(404,{"Content-Type":"application/json"});
         res.end(JSON.stringify({"message":"Not Found!"}))
