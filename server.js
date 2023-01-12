aad94bd7e2ec0ca70e758466a5ca95bec6b010e2
Fulya <fulya.ertay@gmail.com>
2023-01-12 22:38:26 +0300

    send datas on server

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
-const http=require("http");
-const server=http.createServer((req,res)=>{
-    console.log(req.url);
 
+const http=require("http");
+const datas=require("./data/userData.json")
+const server=http.createServer((req,res)=>{
+    // // console.log(req.url);
+    // res.statusCode=200;
+    // res.setHeader("Content-Type","text/html")
+    // res.write("<h1>Hello NodeJS</h1>")
+    // res.end();
+    res.writeHead(200,{"Content-Type":"application/json"});
+    res.end(JSON.stringify(datas))
 })
 const PORT=process.env.PORT || 5000;
 server.listen(PORT,()=>{
